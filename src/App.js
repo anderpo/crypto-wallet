fbd6b56b9f955e5f2dd44a9c5113b503151d28ae
anderpo <potapandre@gmail.com>
2019-10-28 23:28:07 +0300

    minor fix

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import styled from "styled-components";
 
-const Title = styled.h2`
+const Text = styled.h2`
   color: red;
 `;
 
 const Title = () => {
-  return <TItle />;
+  return <Text>Tech challenge</Text>;
 };
 
 export default Title;
