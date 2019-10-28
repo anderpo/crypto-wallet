349acaea8172a38c45591095054089d2f57e2c27
anderpo <potapandre@gmail.com>
2019-10-28 23:26:05 +0300

    configured build and dev webpack

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const Title = styled.h2`
+  color: red;
+`;
+
+const Title = () => {
+  return <TItle />;
+};
+
+export default Title;
