d5d637f7a35b390d3cd01ff1788f5cd72c002c84
anderpo <potapandre@gmail.com>
2019-10-29 23:09:01 +0300

    fix sizes of header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 
 const Header = styled.div`
   display: flex;
-  padding: 0 35px;
+  padding: 0 10px;
   justify-content: space-between;
   align-items: center;
-  height: 100px;
+  height: 70px;
   width: 100%;
 `;
 
