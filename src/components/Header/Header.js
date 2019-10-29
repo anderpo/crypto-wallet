46961d420baf87a6ed20d4f37c4b663bff3d1b98
anderpo <potapandre@gmail.com>
2019-10-29 22:53:46 +0300

    add header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Header = styled.div`
+  display: flex;
+  padding: 0 35px;
+  justify-content: space-between;
+  align-items: center;
+  height: 100px;
+  width: 100%;
+`;
+
+export default Header;
---
+++ src/components/Search/Search.js
@@ -0,0 +1,8 @@
+import React from "react";
+import SearchIcon from "./../../assets/icons/search";
+
+const Search = () => {
+  return <SearchIcon width={17} height={20} />;
+};
+
+export default Search;
