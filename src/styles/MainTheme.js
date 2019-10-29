e6eff713fe8f70de34ee4450e817cc736a6ac12a
anderpo <potapandre@gmail.com>
2019-10-30 00:30:41 +0300

    add text color

--- src/styles/MainTheme.js
+++ src/styles/MainTheme.js
@@ -2,6 +2,7 @@ const MainTheme = {
   gradientFirstColor: "#3f414e",
   gradientSecondColor: "#1f1f2b",
   santasGray: "#9fa0a5",
+  spunPearl: "#a8aab6",
   white: "#ffffff",
   caribbeanGreen: "#0cdc86",
   vividTangerine: "#ff8786",
