b6e884b967098e939b9efc8304ee6e529a8461d6
anderpo <potapandre@gmail.com>
2019-10-30 15:22:49 +0300

    get rate exchange from api

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,22 +1,46 @@
 import React from "react";
+import { connect } from "react-redux";
 import Header from "../components/Header/Header";
 import Search from "./../components/Search/Search";
 import Notification from "./../components/Notification/Notification";
 import TotalBalance from "./../components/TotalBalance/TotalBalance";
 import HomePageWrapper from "../components/HomePageWrapper/HomePageWrapper";
 import CurrencyList from "../components/CurrencyList/CurrencyList";
+import { getRateExchange } from "../actions/WalletAcions";
 
-const HomePage = () => {
-  return (
-    <HomePageWrapper>
-      <Header>
-        <Search />
-        <Notification />
-      </Header>
-      <TotalBalance />
-      <CurrencyList />
-    </HomePageWrapper>
-  );
+class HomePage extends React.Component {
+  componentDidMount() {
+    const { todayRateExchange, onGetRateExchange } = this.props;
+    onGetRateExchange(todayRateExchange.map(curr => curr.name).join(","));
+  }
+
+  render() {
+    return (
+      <HomePageWrapper>
+        <Header>
+          <Search />
+          <Notification />
+        </Header>
+        <TotalBalance />
+        <CurrencyList />
+      </HomePageWrapper>
+    );
+  }
+}
+
+const mapStateToProps = ({ wallet }) => {
+  return {
+    todayRateExchange: wallet.todayRateExchange
+  };
 };
 
-export default HomePage;
+const mapDispatchToProps = dispatch => {
+  return {
+    onGetRateExchange: params => dispatch(getRateExchange(params))
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(HomePage);
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,5 +1,45 @@
-const initialState = {};
+import {
+  GET_RATE_EXCHANGE,
+  GET_RATE_EXCHANGE_SUCCESS,
+  GET_RATE_EXCHANGE_FAIL
+} from "./../actions/WalletAcions";
 
-export function walletReducer(state = initialState) {
-  return state;
+const initialState = {
+  todayRateExchange: [
+    { fullName: "Bitcoin", name: "BTC", price: { USD: 0 } },
+    { fullName: "Ethereum", name: "ETH", price: { USD: 0 } },
+    { fullName: "Ripple", name: "XRP", price: { USD: 0 } }
+  ],
+};
+
+function updateTodayRateExhange(data, rateExchange) {
+  return data.map(c => {
+    return {
+      ...c,
+      price: {
+        USD: rateExchange[c.name].USD
+      }
+    };
+  });
+}
+
+export function walletReducer(state = initialState, action) {
+  switch (action.type) {
+    case GET_RATE_EXCHANGE:
+      return { ...state, loading: true, error: "" };
+
+    case GET_RATE_EXCHANGE_SUCCESS:
+      return {
+        ...state,
+        todayRateExchange: updateTodayRateExhange(state.todayRateExchange, action.payload),
+        loading: false,
+        error: ""
+      };
+
+    case GET_RATE_EXCHANGE_FAIL:
+      return { ...state, error: action.payload.message, loading: false };
+
+    default:
+      return state;
+  }
 }
