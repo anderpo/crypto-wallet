4407636cf1eda6a30be6c12fc7423cb6eee14b64
anderpo <potapandre@gmail.com>
2019-10-30 11:37:44 +0300

    connected redux to the app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
+import { Provider as ReduxProvider } from "react-redux";
 import MainTheme from "./styles/MainTheme";
 import GlobalStyle from "./styles/style";
 import GradientWrapper from "./components/GradientWrapper/GradientWrapper";
 import HomePage from "./pages/HomePage";
+import { store } from "./store/configureStore";
 
 const App = () => {
   return (
-    <ThemeProvider theme={MainTheme}>
-      <React.Fragment>
-        <GlobalStyle font={MainTheme.fontFamily} />
-        <BrowserRouter>
-          <GradientWrapper>
-            <Switch>
-              <Route exact path="/" component={HomePage} />
-            </Switch>
-          </GradientWrapper>
-        </BrowserRouter>
-      </React.Fragment>
-    </ThemeProvider>
+    <ReduxProvider store={store}>
+      <ThemeProvider theme={MainTheme}>
+        <React.Fragment>
+          <GlobalStyle font={MainTheme.fontFamily} />
+          <BrowserRouter>
+            <GradientWrapper>
+              <Switch>
+                <Route exact path="/" component={HomePage} />
+              </Switch>
+            </GradientWrapper>
+          </BrowserRouter>
+        </React.Fragment>
+      </ThemeProvider>
+    </ReduxProvider>
   );
 };
 
---
+++ src/reducers/wallet.js
@@ -0,0 +1,5 @@
+const initialState = {};
+
+export function walletReducer(state = initialState) {
+  return state;
+}
